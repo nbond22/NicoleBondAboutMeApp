085ffa4091932c1bb055656dce9510ce4b08ea8f
Nicole Bond <nicbond22@gmail.com>
2024-04-21 20:11:17 -0400

    Initial Commit

---
+++ NicoleBondAboutMeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NicoleBondAboutMeApp
+//
+//  Created by Scholar on 4/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NicoleBondAboutMeApp/NicoleBondAboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NicoleBondAboutMeAppApp.swift
+//  NicoleBondAboutMeApp
+//
+//  Created by Scholar on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NicoleBondAboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
